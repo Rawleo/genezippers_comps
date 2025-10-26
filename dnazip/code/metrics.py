d892329fc7e13cb7d054a6a9521b63a9d109d4bc
arroyoruizj <arroyoruizj@carleton.edu>
2025-10-25 23:59:45 -0500

    Changed function names

--- dnazip/code/metrics.py
+++ dnazip/code/metrics.py
@@ -1,22 +1,22 @@
 from constants import *
 import os
 
-def get_file_size(file_path):
+def file_size(file_path):
 
     return os.path.getsize(file_path)
 
 
 def compression_ratio(orig_file_path, enc_file_path):
 
-    orig_file_size = get_file_size(orig_file_path)
-    enc_file_size = get_file_size(enc_file_path)
+    orig_file_size = file_size(orig_file_path)
+    enc_file_size = file_size(enc_file_path)
 
     return round(enc_file_size / orig_file_size, 3)
 
 
-def space_saving(orig_file_path, enc_file_path):
+def space_savings(orig_file_path, enc_file_path):
 
-    compression_ratio =  compression_ratio(orig_file_path, enc_file_path)
+    comp_ratio = compression_ratio(orig_file_path, enc_file_path)
 
-    return round(1 - compression_ratio, 3)
+    return round(1 - comp_ratio, 3)
 

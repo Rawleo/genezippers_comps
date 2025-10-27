a1041f5071b07bfaf460e9985f260fa4ec3238fd
gavinsaxer <gavin.saxer@gmail.com>
2025-10-26 19:12:43 -0500

    .

--- biocompress_1/config.py
+++ biocompress_1/config.py
@@ -3,6 +3,7 @@ DNA_FILE_PATH = "./data/"
 DNA_FILE_TXT = DNA_FILE + ".txt"
 DNA_FILE_FA = DNA_FILE + ".fa"
 HEIGHT = 8
+COMPARE_LENGTH = 50
 
 with open(DNA_FILE_PATH+DNA_FILE_TXT, "r") as file:
        CONTENT = file.read()

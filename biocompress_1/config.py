0bd0846fe9025a57281b66a43fe0401ba9806362
gavinsaxer <gavin.saxer@gmail.com>
2025-11-02 18:10:51 -0600

    ran on ecoli

--- biocompress_1/config.py
+++ biocompress_1/config.py
@@ -1,8 +1,8 @@
-DNA_FILE = "chr21"
-DNA_FILE_PATH = "./data/"
+DNA_FILE = "ecoli"
+DNA_FILE_PATH = "../dnazip/data/chr/"
 DNA_FILE_TXT = DNA_FILE + ".txt"
 DNA_FILE_FA = DNA_FILE + ".fa"
-HEIGHT = 10
+HEIGHT = 8
 COMPARE_LENGTH = 50
 
 with open(DNA_FILE_PATH+DNA_FILE_TXT, "r") as file:

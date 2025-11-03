b688b827e66042af54ccef688aa5b944429464f5
Rawleo <sonryan50@gmail.com>
2025-11-02 22:02:35 -0600

    Cleaning up huffman encoding

--- huffman_coding/config.py
+++ huffman_coding/config.py
@@ -1,22 +1,22 @@
 from pathlib import Path  
 
-FULL = False
-K_MER_SIZE = 4
-BASE_PATH = Path(__file__).resolve().parent.parent
-OUTPUT_DIR = BASE_PATH / 'huffman_coding' / 'output'
-GENOME_DIR = BASE_PATH / 'dnazip' / 'data' /'chr'
+FULL          = False
+K_MER_SIZE    = 4
+BASE_PATH     = Path(__file__).resolve().parent.parent
+OUTPUT_DIR    = BASE_PATH / 'huffman_coding' / 'output'
+GENOME_DIR    = BASE_PATH / 'dnazip' / 'data' /'chr'
 
 if (FULL):
-    GENOME_BIN = OUTPUT_DIR / f'ENCODED_GENOME.bin'
-    GENOME_FILE = GENOME_DIR / 'genome.txt'
-    DECODED_FILE = OUTPUT_DIR / 'DECODED_GENOME.txt'
-    HUFFMAN_TREE = OUTPUT_DIR / 'HUFFMAN_TREE_GENOME.txt'
+    GENOME_BIN    = OUTPUT_DIR / 'ENCODED_GENOME.bin'
+    GENOME_FILE   = GENOME_DIR / 'genome.txt'
+    DECODED_FILE  = OUTPUT_DIR / 'DECODED_GENOME'
+    HUFFMAN_TREE  = OUTPUT_DIR / 'HUFFMAN_TREE_GENOME.txt'
 else:
-    CHR = 'chr21'
-    GENOME_BIN = OUTPUT_DIR / f'ENCODED_{CHR}'
-    GENOME_FILE = GENOME_DIR / f'{CHR}.txt'
-    DECODED_FILE = OUTPUT_DIR / f'DECODED_{CHR}.txt'
-    HUFFMAN_TREE = OUTPUT_DIR / f'HUFFMAN_TREE_{CHR}.txt'
+    CHR           = 'chr19'
+    GENOME_BIN    = OUTPUT_DIR / f'ENCODED_{CHR}.bin'
+    GENOME_FILE   = GENOME_DIR / f'{CHR}.txt'
+    DECODED_FILE  = OUTPUT_DIR / f'DECODED_{CHR}.txt'
+    HUFFMAN_TREE  = OUTPUT_DIR / f'HUFFMAN_TREE_{CHR}.txt'
     
 ###
 # Array of Chromosomes

16760c18a2c1757d7e5ac2358b92dfb39cf89a5d
gavinsaxer <gavin.saxer@gmail.com>
2025-11-02 20:09:04 -0600

    a few small changes

--- biocompress_1/preprocessor.py
+++ biocompress_1/preprocessor.py
@@ -1,12 +1,13 @@
 from config import CONTENT, DNA_FILE, DNA_FILE_PATH
 
+
 open(DNA_FILE_PATH + DNA_FILE + ".txt", "w").close()
 output_file = open(DNA_FILE_PATH + DNA_FILE + ".txt", "a", encoding="utf-8")
 
 def main():
     for i in CONTENT:
-        if i in ['A', 'C', 'T', 'G']:
-            output_file.write(i)
+        if i.upper() in ['A', 'C', 'T', 'G']:
+            output_file.write(i.upper())
             
 
 if __name__ == "__main__":
